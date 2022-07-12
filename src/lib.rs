6a29b43c6be9fd7a76401fef2568741eabc2dfb5
Guilherme Salustiano <guissalustiano@gmail.com>
2022-07-12 17:30:21 -0300

    parse mannualy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::convert::identity;
+
 pub fn asm2mvn(asm: &str) -> &str {
     Code::parse(asm).to_mvn()
 }
@@ -15,6 +17,13 @@ enum Operand {
     Simbolic(Label),
     Numeric(u16),
 }
+impl Operand {
+    pub fn parse(src: &str) -> Self{
+        u16::from_str_radix(src, 16).map_or_else(|_| Self::simbolic(src), Self::numeric)
+    }
+    fn simbolic(value: &str) -> Self { Operand::Simbolic(Label::new(value)) }
+    fn numeric(value: u16) -> Self { Operand::Numeric(value) }
+}
 
 #[derive(Debug, PartialEq)]
 enum Mneumonic {
@@ -22,6 +31,7 @@ enum Mneumonic {
     Add(Operand),
     Load(Operand),
     Store(Operand),
+    Address(Operand),
 }
 impl Mneumonic {
     pub fn const_val(value: u16) -> Self {
@@ -38,6 +48,17 @@ impl Mneumonic {
 
     pub fn store(label: &str) -> Self {
         Mneumonic::Store(Operand::Simbolic(Label::new(label)))
+    }
+
+    pub fn parse(mneumonic: &str, arg: Operand) -> Self {
+        match mneumonic {
+            "K" => Mneumonic::Constant(arg),
+            "LD" => Mneumonic::Load(arg),
+            "AD" => Mneumonic::Add(arg),
+            "MM" => Mneumonic::Store(arg),
+            "@"  => Mneumonic::Address(arg),
+            _ => panic!("Invalid mneumonic")
+        }
     }
 }
 
@@ -60,8 +81,41 @@ impl CodeLine {
         CodeLine::NextAddress(Address(addr))
     }
 
-    pub fn parse(line: &str) -> Self {
-        todo!()
+    // TODO: reduce unwrap
+    pub fn parse(line: &str) -> Option<Self> {
+        let line = line.trim();
+        if line.is_empty() {
+            return None
+        }
+        
+        let mut terms = line.split_whitespace();
+        let first_term = terms.next();
+        let second_term = terms.next();
+        let third_term = terms.next();
+        let forth_term = terms.next();
+
+        if first_term.is_none() {
+            return None
+        }
+
+        if !forth_term.is_none() && forth_term != Some("--") {
+            let term = forth_term.unwrap();
+            panic!("Error on parse {}", term);
+        }
+
+        let code_line = if let Some(third_term_value) = third_term {
+            Self::parse_splited(first_term, second_term.unwrap(), third_term_value) 
+        } else {
+            Self::parse_splited(None, first_term.unwrap(), second_term.unwrap()) 
+        };
+        Some(code_line)
+    }
+
+    fn parse_splited(lab: Option<&str>, mneu: &str, arg: &str) -> Self {
+        let label = lab.map(Label::new);
+        let operand = Operand::parse(arg);
+        let mneumonic = Mneumonic::parse(mneu, operand);
+        Self::Instruction(label, mneumonic)
     }
 }
 
@@ -74,7 +128,7 @@ impl Code {
         Self { lines}
     }
     fn parse(code: &str) -> Self {
-        Code::new(code.lines().map(CodeLine::parse).collect())
+        Code::new(code.lines().map(CodeLine::parse).filter_map(identity).collect())
     }
     fn to_mvn(&self) -> &'static str {
         todo!()
@@ -88,13 +142,13 @@ mod tests {
 
     #[test]
     fn should_returns_line_code_given_asm_mneumonic() {
-        assert_eq!(CodeLine::parse("VAL_A   K   /0001"), CodeLine::with_label("VAL_B", Mneumonic::const_val(2)));
-        assert_eq!(CodeLine::parse("VAL_B   K   /0002"), CodeLine::with_label("RESLT", Mneumonic::const_val(0)));
-        assert_eq!(CodeLine::parse("RESLT   K   /0000"), CodeLine::with_label("MAIN", Mneumonic::load("VAL_A")));
-        assert_eq!(CodeLine::parse("@   /0100"), CodeLine::address(0x100));
-        assert_eq!(CodeLine::parse("MAIN    LD  VAL_A"), CodeLine::mneumonic(Mneumonic::add("VAL_B")));
-        assert_eq!(CodeLine::parse("AD  VAL_B"), CodeLine::mneumonic(Mneumonic::add("VAL_B")));
-        assert_eq!(CodeLine::parse("MM  RESLT"), CodeLine::mneumonic(Mneumonic::store("RESLT")));
+        assert_eq!(CodeLine::parse("VAL_A   K   /0001"), Some(CodeLine::with_label("VAL_B", Mneumonic::const_val(2))));
+        assert_eq!(CodeLine::parse("VAL_B   K   /0002"), Some(CodeLine::with_label("RESLT", Mneumonic::const_val(0))));
+        assert_eq!(CodeLine::parse("RESLT   K   /0000"), Some(CodeLine::with_label("MAIN", Mneumonic::load("VAL_A"))));
+        assert_eq!(CodeLine::parse("@   /0100"),         Some(CodeLine::address(0x100)));
+        assert_eq!(CodeLine::parse("MAIN    LD  VAL_A"), Some(CodeLine::mneumonic(Mneumonic::add("VAL_B"))));
+        assert_eq!(CodeLine::parse("AD  VAL_B"),         Some(CodeLine::mneumonic(Mneumonic::add("VAL_B"))));
+        assert_eq!(CodeLine::parse("MM  RESLT"),         Some(CodeLine::mneumonic(Mneumonic::store("RESLT"))));
     }
 
     #[test]
