42480fc9a5653cc3c01119f6045444624fa47ced
Guilherme Salustiano <guissalustiano@gmail.com>
2022-07-12 01:33:14 -0300

    create intermediary types

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,80 @@
-pub fn asm2mvn(_asm: &str) -> &str {
-    todo!("Not implemented yet")
+pub fn asm2mvn(asm: &str) -> &str {
+    Code::parse(asm).to_mvn()
+}
+
+#[derive(Debug, PartialEq)]
+struct Label(String);
+impl Label {
+    pub fn new(value: &str) -> Self {
+        Self(value.to_string())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Operand {
+    Simbolic(Label),
+    Numeric(u16),
+}
+
+#[derive(Debug, PartialEq)]
+enum Mneumonic {
+    Constant(Operand),
+    Add(Operand),
+    Load(Operand),
+    Store(Operand),
+}
+impl Mneumonic {
+    pub fn const_val(value: u16) -> Self {
+        Mneumonic::Constant(Operand::Numeric(value))
+    }
+
+    pub fn add(label: &str) -> Self {
+        Mneumonic::Add(Operand::Simbolic(Label::new(label)))
+    }
+
+    pub fn load(label: &str) -> Self {
+        Mneumonic::Load(Operand::Simbolic(Label::new(label)))
+    }
+
+    pub fn store(label: &str) -> Self {
+        Mneumonic::Store(Operand::Simbolic(Label::new(label)))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Address(u16);
+
+#[derive(Debug, PartialEq)]
+enum CodeLine {
+    Instruction(Option<Label>, Mneumonic),
+    NextAddress(Address)
+}
+impl CodeLine {
+    pub fn with_label (label: &str, mneu: Mneumonic) -> Self {
+        CodeLine::Instruction(Some(Label::new(label)), mneu)
+    }
+    pub fn mneumonic (mneu: Mneumonic) -> Self {
+        CodeLine::Instruction(None, mneu)
+    }
+    pub fn address (addr: u16) -> Self {
+        CodeLine::NextAddress(Address(addr))
+    }
+}
+
+#[derive(Debug, PartialEq)]
+struct Code {
+    pub lines: Vec<CodeLine>
+}
+impl Code {
+    fn new(lines: Vec<CodeLine>) -> Self {
+        Self { lines}
+    }
+    fn parse(_code: &str) -> Self {
+        todo!()
+    }
+    fn to_mvn(&self) -> &'static str {
+        todo!()
+    }
 }
 
 #[cfg(test)]
@@ -8,6 +83,53 @@ mod tests {
     use indoc::indoc;
 
     #[test]
+    fn should_returns_code_given_asm() {
+        let input = indoc! {"
+            VAL_A   K   /0001
+            VAL_B   K   /0002
+            RESLT   K   /0000
+
+                    @   /0100
+            MAIN    LD  VAL_A
+                    AD  VAL_B
+                    MM  RESLT
+        "};
+        let expected = Code::new(vec![
+             CodeLine::with_label("VAL_A", Mneumonic::const_val(1)),
+             CodeLine::with_label("VAL_B", Mneumonic::const_val(2)),
+             CodeLine::with_label("RESLT", Mneumonic::const_val(0)),
+             CodeLine::address(0x100),
+             CodeLine::with_label("MAIN", Mneumonic::load("VAL_A")),
+             CodeLine::mneumonic(Mneumonic::add("VAL_B")),
+             CodeLine::mneumonic(Mneumonic::store("RESLT")),
+        ]);
+        assert_eq!(Code::parse(input), expected);
+    }
+
+    #[test]
+    fn should_returns_mvn_given_code() {
+        let input = Code::new(vec![
+             CodeLine::with_label("VAL_A", Mneumonic::const_val(1)),
+             CodeLine::with_label("VAL_B", Mneumonic::const_val(2)),
+             CodeLine::with_label("RESLT", Mneumonic::const_val(0)),
+             CodeLine::address(0x100),
+             CodeLine::with_label("MAIN", Mneumonic::load("VAL_A")),
+             CodeLine::mneumonic(Mneumonic::add("VAL_B")),
+             CodeLine::mneumonic(Mneumonic::store("RESLT")),
+        ]);
+        let expected = indoc! {"
+            00000001
+            00020002
+            00040000
+            01008000
+            01024002
+            01049004
+        "};
+
+        assert_eq!(input.to_mvn(), expected);
+    }
+
+        #[test]
     fn should_returns_mvn_given_asm() {
         let input = indoc! {"
             VAL_A   K   /0001
