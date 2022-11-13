dff9fd56a90d454ec25d81f921589dd8e2b9de46
tomaz-suller <tomaz.suller@usp.br>
2022-11-13 15:07:36 -0300

    Add base writer and use it in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
-use std::io;
-use std::io::Read;
+use indoc::indoc;
 
 use mvn::parser::lines::Lines;
+use mvn::processor::address::AddressedLines;
+use mvn::writer::base::print;
 
-fn main() -> io::Result<()> {
-    let mut input = String::new();
-    io::stdin().read_to_string(&mut input)?;
-
-    let (_, lines) = Lines::parse(&input).unwrap();
-
-    print!("{:?}", lines);
-    Ok(())
+fn main() {
+    let program = Lines::parse(indoc! {"
+        < IMPORTED
+        > RESERVE
+                JP  MAIN
+        TWO     K   /2 ; This is an inline comment
+        FOUR    K   /4
+        RESERVE $   /4
+        ; This is a comment
+        MAIN    LD  TWO
+                AD  FOUR
+                MM  RESERVE
+                HM  /0
+        # MAIN
+    "}).unwrap().1;
+    let addresses_program = AddressedLines::parse(program);
+    print(&addresses_program);
 }
