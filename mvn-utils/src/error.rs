c609973f2df690858177d986746a4d3e77f1ae2a
tomaz-suller <tomaz.suller@usp.br>
2022-12-30 23:30:23 -0300

    Add hexadecimal and error helpers to mvn-utils

---
+++ mvn-utils/src/error.rs
@@ -0,0 +1,75 @@
+use nom;
+use nom_locate::LocatedSpan;
+
+pub type Span<'a> = LocatedSpan<&'a str>;
+pub type LocatedError<'a> = nom::error::Error<Span<'a>>;
+pub type LocatedIResult<'a, O> = nom::IResult<Span<'a>, O, MvnParseError<'a>>;
+
+/*
+ * Custom error type allows for domain-specific error messages,
+ * located using `nom_locate` `Span`s for later annotation
+ *
+ *
+ * From: https://iximiuz.com/en/posts/rust-writing-parsers-with-nom/
+ */
+
+#[derive(Debug, PartialEq)]
+pub struct MvnParseError<'a> {
+    span: Span<'a>,
+    message: Option<String>,
+}
+
+impl<'a> MvnParseError<'a> {
+    pub fn new(message: String, span: Span<'a>) -> Self {
+        Self { span, message: Some(message) }
+    }
+
+    pub fn span(&self) -> &Span { &self.span }
+
+    pub fn line(&self) -> u32 { self.span().location_line() }
+
+    pub fn offset(&self) -> usize { self.span().location_offset() }
+}
+
+// That's what makes it nom-compatible.
+impl<'a> nom::error::ParseError<Span<'a>> for MvnParseError<'a> {
+    fn from_error_kind(input: Span<'a>, kind: nom::error::ErrorKind) -> Self {
+        Self::new(format!("parse error {:?}", kind), input)
+    }
+
+    fn append(_input: Span<'a>, _kind: nom::error::ErrorKind, other: Self) -> Self {
+        other
+    }
+
+    fn from_char(input: Span<'a>, c: char) -> Self {
+        Self::new(format!("unexpected character '{}'", c), input)
+    }
+}
+
+#[macro_export]
+macro_rules! error_or {
+    ($result:expr, $input:expr, $message:literal) => {
+        match $result {
+            Ok((rest, content)) => Ok((rest, content)),
+            Err(nom::Err::Error(_)) => Err(nom::Err::Error(utils::error::MvnParseError::new(
+                $message.to_owned(),
+                $input,
+            ))),
+            Err(e) => Err(e),
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! failure_or {
+    ($result:expr, $input:expr, $message:literal) => {
+        match $result {
+            Ok((rest, content)) => Ok((rest, content)),
+            Err(nom::Err::Error(_)) => Err(nom::Err::Failure(utils::error::MvnParseError::new(
+                $message.to_owned(),
+                $input,
+            ))),
+            Err(e) => Err(e),
+        }
+    };
+}
--- mvn-utils/src/lib.rs
+++ mvn-utils/src/lib.rs
@@ -1,3 +1,26 @@
+pub mod error;
+
+use error::{Span, MvnParseError};
+use nom::IResult;
+use nom::character::complete::hex_digit1;
+use nom::combinator::{map, recognize};
+use num_traits::Num;
+
+/*
+ * Adapted from https://github.com/Geal/nom/blob/main/doc/nom_recipes.md#hexadecimal
+ */
+pub fn hexadecimal<T: Num>(input: Span) -> IResult<Span<'_>, T, MvnParseError> {
+    map(
+        recognize(hex_digit1),
+        /* Unwrapping is not allowed since `<T as Num>::FromStrRadixErr`
+         * doesn't implement `Debug`. We use `unwrap_or` as a workaround,
+         * knowing the default value will never be used since nom
+         * will return an `Err` if the conversion is not possible
+         */
+        |out: Span| T::from_str_radix(&out, 16).unwrap_or(T::zero())
+    )(input)
+}
+
 pub fn hex_char_to_u8(string: &str) -> u8 {
     let char = string.chars().next().expect("Input string should contain at least one character.");
     if !char.is_ascii_hexdigit() { panic!("Input is not a valid ASCII hex digit."); }
@@ -10,5 +33,21 @@ pub fn hex_char_to_u8(string: &str) -> u8 {
     // We know the char is a hex digit, so no more checks are necessary
     } else { // Lower case letters a..=f
         char - 0x61 + 10
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_parse_valid_hexadecimals() {
+        assert_eq!(hexadecimal::<u8>(Span::new("0")).unwrap().1, 0x0);
+        assert_eq!(hexadecimal::<u8>(Span::new("9")).unwrap().1, 0x9);
+        assert_eq!(hexadecimal::<u8>(Span::new("A")).unwrap().1, 0xA);
+        assert_eq!(hexadecimal::<u8>(Span::new("F")).unwrap().1, 0xF);
+        assert_eq!(hexadecimal::<u8>(Span::new("10")).unwrap().1, 0x10);
+        assert_eq!(hexadecimal::<u8>(Span::new("FF")).unwrap().1, 0xFF);
+        assert_eq!(hexadecimal::<u16>(Span::new("FFFF")).unwrap().1, 0xFFFF);
     }
 }
