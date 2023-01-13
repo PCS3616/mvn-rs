5d5d53600c25e92ada403f7326cbeb0f9cd129fd
tomaz-suller <tomaz.suller@usp.br>
2023-01-13 15:47:18 -0300

    Implement TryFrom for Label

--- mvn-types/src/operand.rs
+++ mvn-types/src/operand.rs
@@ -1,4 +1,5 @@
 use std::fmt;
+use std::convert::{From, TryFrom};
 
 use crate::Label;
 
@@ -8,6 +9,7 @@ pub enum Operand<'a> {
     Numeric(u32),
 }
 
+// FIXME Replace calls to `new_numeric` and `new_symbolic` with `from`
 impl<'a> Operand<'a> {
     pub fn new_numeric(value: u32) -> Self {
         Self::Numeric(value)
@@ -18,21 +20,41 @@ impl<'a> Operand<'a> {
     }
 }
 
-impl<'a> std::convert::From<u32> for Operand<'a> {
+impl From<u32> for Operand<'_> {
     fn from(value: u32) -> Self {
         Self::Numeric(value)
     }
 }
 
-impl<'a> std::convert::From<Label<'a>> for Operand<'a> {
+impl<'a> From<Label<'a>> for Operand<'a> {
     fn from(value: Label<'a>) -> Self {
         Self::Symbolic(value)
     }
 }
 
-impl<'a> std::convert::From<&'a str> for Operand<'a> {
+impl<'a> From<&'a str> for Operand<'a> {
     fn from(value: &'a str) -> Self {
         Self::Symbolic(value.into())
+    }
+}
+
+impl TryFrom<Operand<'_>> for u32 {
+    type Error = &'static str;
+    fn try_from(value: Operand) -> Result<Self, Self::Error> {
+        match value {
+            Operand::Numeric(value) => Ok(value),
+            Operand::Symbolic(_) => Err("operand is not numeric, so cannot be converted to `u16`"),
+        }
+    }
+}
+
+impl<'a> TryFrom<Operand<'a>> for Label<'a> {
+    type Error = &'static str;
+    fn try_from(value: Operand<'a>) -> Result<Self, Self::Error> {
+        match value {
+            Operand::Numeric(_) => Err("operand is not symbolic, so cannot be converted to `Label`"),
+            Operand::Symbolic(label) => Ok(label),
+        }
     }
 }
 
