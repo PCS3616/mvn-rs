ebbea3b884b9bc64fa57aa0e3ca9630440598d71
tomaz-suller <tomaz.suller@usp.br>
2023-01-15 21:21:43 -0300

    Make mneumonics reserved keywords
    `Label`s were allowed to match mneumonics, which didn't allow parsing
    lines properly. This change makes `Label`s matching any mneumonic
    invalid, and tests that is the case.

--- mvn-assembler/src/parser/label.rs
+++ mvn-assembler/src/parser/label.rs
@@ -1,4 +1,4 @@
-use nom::combinator::map;
+use nom::combinator::{map, not};
 use utils::error_or;
 
 use super::error::{LocatedIResult, Span};
@@ -7,11 +7,13 @@ use super::Parse;
 
 impl<'a> Parse<'a> for types::Label<'a> {
     fn parse(input: Span<'a>) -> LocatedIResult<'a, Self> {
-        let label = map(identifier, |out: &str| Self::new(out))(input);
+        let label = not(types::Instruction::parse)(input).and_then(
+            |(input, _)| map(identifier, |out: &str| Self::new(out))(input)
+        );
         error_or!(
             label,
             input,
-            "invalid label; perhaps you started with a number?"
+            "invalid label; perhaps you used non-ASCII characters or started with a number"
         )
     }
 }
@@ -20,8 +22,47 @@ impl<'a> Parse<'a> for types::Label<'a> {
 mod tests {
     use pretty_assertions::assert_eq;
     use types::Label;
+    use types::mneumonic::*;
 
     use super::*;
+
+    fn normal_mneumonics() -> [NormalMneumonic; 17] {
+        [
+            NormalMneumonic::Jump,
+            NormalMneumonic::JumpIfZero,
+            NormalMneumonic::JumpIfNegative,
+            NormalMneumonic::LoadValue,
+            NormalMneumonic::Add,
+            NormalMneumonic::Subtract,
+            NormalMneumonic::Multiply,
+            NormalMneumonic::Divide,
+            NormalMneumonic::Load,
+            NormalMneumonic::Memory,
+            NormalMneumonic::Subroutine,
+            NormalMneumonic::ReturnFromSubrotine,
+            NormalMneumonic::HaltMachine,
+            NormalMneumonic::GetData,
+            NormalMneumonic::PutData,
+            NormalMneumonic::OperatingSystem,
+            NormalMneumonic::SetConstant,
+        ]
+    }
+
+    fn positional_mneumonics() -> [PositionalMneumonic; 4] {
+        [
+            PositionalMneumonic::SetAbsoluteOrigin,
+            PositionalMneumonic::SetRelocatableOrigin,
+            PositionalMneumonic::ReserveMemory,
+            PositionalMneumonic::SetEnd,
+        ]
+    }
+
+    fn relational_mneumonics() -> [RelationalMneumonic; 2] {
+        [
+            RelationalMneumonic::Export,
+            RelationalMneumonic::Import,
+        ]
+    }
 
     #[test]
     fn should_parse_label() {
@@ -31,5 +72,26 @@ mod tests {
             assert_eq!(Label::parse(Span::new(input)).unwrap().1, output,);
         }
         assert!(Label::parse(Span::new("1V")).is_err());
+    }
+
+    #[test]
+    fn should_not_parse_normal_mneumonic() {
+        for mneumonic in normal_mneumonics().into_iter() {
+            assert!(Label::parse(mneumonic.to_string().as_str().into()).is_err());
+        }
+    }
+
+    #[test]
+    fn should_not_parse_positional_mneumonic() {
+        for mneumonic in positional_mneumonics().into_iter() {
+            assert!(Label::parse(mneumonic.to_string().as_str().into()).is_err());
+        }
+    }
+
+    #[test]
+    fn should_not_parse_relational_mneumonic() {
+        for mneumonic in relational_mneumonics().into_iter() {
+            assert!(Label::parse(mneumonic.to_string().as_str().into()).is_err());
+        }
     }
 }
