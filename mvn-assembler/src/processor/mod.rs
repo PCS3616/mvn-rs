bb27f43969162a8f7fa39abf8b8781448f52d595
tomaz-suller <tomaz.suller@usp.br>
2023-01-22 11:17:43 -0300

    Add process function to processor module

--- mvn-assembler/src/processor/mod.rs
+++ mvn-assembler/src/processor/mod.rs
@@ -1,1 +1,18 @@
-pub mod address;
+pub(crate) mod address;
+mod validator;
+
+use nom;
+use types::Program;
+use utils::error::MvnParseError;
+
+use crate::parser::Parse;
+use crate::processor::address::{AddressedProgram, LabelMap};
+
+use validator::validate;
+
+pub fn process(program: &str) -> Result<(AddressedProgram, LabelMap), nom::Err<MvnParseError>> {
+    let (_, program) = Program::parse(program.into())?;
+    let addressed_program = AddressedProgram::process(program);
+    let label_map = addressed_program.map_labels();
+    validate(&addressed_program, &label_map).map(|_| (addressed_program, label_map))
+}
