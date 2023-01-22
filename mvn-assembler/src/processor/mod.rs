eb7fc802c2a002f9eb4b7ff9d8e191f53f1a44c0
tomaz-suller <tomaz.suller@usp.br>
2023-01-22 19:02:31 -0300

    Add MvnReportError type

--- mvn-assembler/src/processor/mod.rs
+++ mvn-assembler/src/processor/mod.rs
@@ -5,6 +5,31 @@ use nom;
 use types::Program;
 use utils::error::MvnParseError;
 
+use std::convert::From;
+
+use types::Position;
+
+#[derive(Debug)]
+pub struct MvnReportError {
+    pub position: Position,
+    pub message: Option<String>,
+}
+
+impl From<MvnParseError<'_>> for MvnReportError {
+    fn from(value: MvnParseError) -> Self {
+        Self {
+            position: value.span.into(),
+            message: value.message,
+        }
+    }
+}
+
+impl MvnReportError {
+    fn new(position: Position, message: Option<String>) -> Self {
+        Self { position, message }
+    }
+}
+
 use crate::parser::Parse;
 use crate::processor::address::{AddressedProgram, LabelMap};
 
