1daf1fe90567896a7339990ec9ad09133a4452e4
tomaz-suller <tomaz.suller@usp.br>
2023-01-23 13:44:22 -0300

    Modify processor to use Token

--- mvn-linker/src/processor/mod.rs
+++ mvn-linker/src/processor/mod.rs
@@ -1,6 +1,6 @@
 use std::collections::BTreeMap;
 use types::{Operand, Operation};
-use types::{Label, Instruction, mneumonic::RelationalMneumonic};
+use types::{Label, Instruction, Token, mneumonic::RelationalMneumonic};
 
 use crate::parser::Position;
 use crate::parser::{program::AddressedProgram, line::AddressedLine, Relocate};
@@ -11,7 +11,7 @@ type ExportMap<'a> = BTreeMap<Label<'a>, Position>;
 pub fn process_programs(programs: Vec<AddressedProgram>) -> AddressedProgram {
     let mut exports: ExportMap = BTreeMap::new();
     let mut processed_programs: Vec<AddressedProgram> = Vec::new();
-    let mut base = 0u16;
+    let mut base = 0u32;
     for program in programs {
         let program = process_program(base, program,  &mut exports);
         base = find_latest_position(&program) + 0x2;
@@ -42,8 +42,9 @@ fn replace_imported_operands_with_positions<'a>(program: AddressedProgram<'a>, e
 
 fn replace_imported_operand_line_with_position<'a>(line: AddressedLine<'a>, exports: &ExportMap<'a>) -> AddressedLine<'a> {
     if line.address.properties.operand_imported {
-        let operand = replace_imported_operand_with_position(line.operation.operand, exports);
-        let operation = Operation {operand, ..line.operation};
+        let operand = replace_imported_operand_with_position(line.operation.operand.value, exports);
+        let operand = Token::new(line.operation.operand.position, operand);
+        let operation = Operation { operand, ..line.operation};
         AddressedLine { operation, ..line }
     } else {
         line
@@ -59,7 +60,7 @@ fn replace_imported_operand_with_position<'a>(operand: Operand, exports: &Export
     }
 }
 
-fn find_latest_position(program: &AddressedProgram) -> u16 {
+fn find_latest_position(program: &AddressedProgram) -> Position {
     let line = program.lines.iter().max_by_key(|line| line.address.position);
     match line {
         Some(line) => line.address.position,
@@ -97,7 +98,8 @@ fn replace_imported_operands_with_labels<'a>(program: AddressedProgram<'a>, impo
 
 fn replace_imported_operand_line_with_label<'a>(line: AddressedLine<'a>, imports: &ImportMap<'a>) -> AddressedLine<'a> {
     if line.address.properties.operand_imported {
-        let operand = replace_imported_operand_with_label(line.operation.operand, imports);
+        let operand = replace_imported_operand_with_label(line.operation.operand.value, imports);
+        let operand = Token::new(line.operation.operand.position, operand);
         let operation = Operation {operand, ..line.operation};
         AddressedLine { operation, ..line }
     } else {
@@ -106,7 +108,7 @@ fn replace_imported_operand_line_with_label<'a>(line: AddressedLine<'a>, imports
 }
 
 fn replace_imported_operand_with_label<'a>(operand: Operand, imports: &ImportMap<'a>) -> Operand<'a> {
-    let operand: u16 = operand.try_into().unwrap();
+    let operand: u32 =  operand.try_into().unwrap();
     match imports.get(&operand) {
         Some(label) => Operand::new_symbolic(label.clone()),
         // TODO Add error treatment
@@ -120,8 +122,8 @@ fn replace_imported_operand_with_label<'a>(operand: Operand, imports: &ImportMap
 fn update_export_map<'a>(exports: AddressedProgram<'a>, global_exports: &mut ExportMap<'a>) -> () {
     for line in exports {
         let annotation = line.relational_annotation.unwrap();
-        let label: Label = annotation.operation.operand.try_into().unwrap();
-        let position: u16 = line.operation.operand.try_into().unwrap();
+        let label: Label = annotation.operation.operand.value.try_into().unwrap();
+        let position: u32 = line.operation.operand.value.try_into().unwrap();
         global_exports.insert(label, position);
     }
 }
@@ -130,8 +132,8 @@ fn create_import_map(imports: AddressedProgram) -> ImportMap {
     let mut import_map: Vec<(Position, Label)> = Vec::new();
     for line in imports {
         let annotation = line.relational_annotation.unwrap();
-        let label: Label = annotation.operation.operand.try_into().unwrap();
-        let position: u16 = line.operation.operand.try_into().unwrap();
+        let label: Label = annotation.operation.operand.value.try_into().unwrap();
+        let position: u32 = line.operation.operand.value.try_into().unwrap();
         import_map.push((position, label));
     }
     import_map.into_iter().collect()
@@ -139,7 +141,7 @@ fn create_import_map(imports: AddressedProgram) -> ImportMap {
 
 fn line_is_import(line: &Option<types::Line>) -> bool {
     if let Some(line) = line {
-        if let Instruction::Relational(mneumonic) = &line.operation.instruction {
+        if let Instruction::Relational(mneumonic) = &line.operation.instruction.value {
             return mneumonic == &RelationalMneumonic::Import
         }
     }
