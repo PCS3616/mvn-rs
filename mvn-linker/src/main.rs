b18f4634d5b47b19862a19db0ebba13517c4793e
tomaz-suller <tomaz.suller@usp.br>
2023-01-26 08:14:53 -0300

    Modify processor to allow partial linkage
    Partial linkage means not resolving all symbols from the provided files.

--- mvn-linker/src/main.rs
+++ mvn-linker/src/main.rs
@@ -7,13 +7,15 @@ use mvn_linker::processor::ProgramsProcessor;
 fn main() {
     let main_program = indoc! {"
         1000 0000 ; < _ADD_TWO
+        1000 0001 ; < FOO
         0000 0002 ; > RESULT
         0000 0004 ;         JP  MAIN
         0002 0000 ; RESULT  $   /2
         0004 3003 ; MAIN    LV  /3
         1006 A000 ;         SC  ADD_TWO
-        0008 C000 ;         HM  /0
-        000A 0000 ; # MAIN
+        1008 9001 ;         MM  FOO
+        000A C000 ;         HM  /0
+        000C 0000 ; # MAIN
     "};
     let subroutine_program = indoc! {"
         6100 0102 ; > _ADD_TWO
@@ -32,5 +34,5 @@ fn main() {
     .collect();
     let processor = ProgramsProcessor::new(programs);
 
-    println!("{:#?}", processor.linked_program);
+    println!("{:#?}", processor);
 }
--- mvn-linker/src/processor/mod.rs
+++ mvn-linker/src/processor/mod.rs
@@ -1,8 +1,8 @@
-use std::collections::BTreeMap;
+use std::collections::{BTreeMap, BTreeSet};
 
 use utils::types::Token;
 
-use crate::types::{Label, Operation, AddressPosition, AddressedProgram, AddressedLine};
+use crate::types::{Label, Operation, AddressPosition, AddressedProgram, AddressedLine, Operand};
 use crate::parser::Relocate;
 
 type ImportMap<'a> = BTreeMap<AddressPosition, Label<'a>>;
@@ -11,6 +11,7 @@ type ExportMap<'a> = BTreeMap<Label<'a>, AddressPosition>;
 struct ProgramProcessor<'a> {
     program: AddressedProgram<'a>,
     export_map: ExportMap<'a>,
+    import_map: ImportMap<'a>,
 }
 
 impl<'a> ProgramProcessor<'a> {
@@ -19,8 +20,8 @@ impl<'a> ProgramProcessor<'a> {
         let (imports, exports, instructions) = program.partition();
         let import_map = Self::create_import_map(imports);
         let export_map = Self::create_export_map(exports);
-        let program = Self::replace_imported_operands_with_labels(instructions, import_map);
-        ProgramProcessor { program, export_map }
+        let program = Self::replace_imported_operands_with_labels(instructions, &import_map);
+        ProgramProcessor { program, export_map, import_map }
     }
 
     fn create_import_map(imports: Vec<AddressedLine>) -> ImportMap {
@@ -43,21 +44,20 @@ impl<'a> ProgramProcessor<'a> {
         export_map
     }
 
-    fn replace_imported_operands_with_labels(instructions: Vec<AddressedLine<'a>>, mut import_map: ImportMap<'a>) -> AddressedProgram<'a> {
+    fn replace_imported_operands_with_labels(instructions: Vec<AddressedLine<'a>>, import_map: &ImportMap<'a>) -> AddressedProgram<'a> {
         let mut lines:  Vec<AddressedLine> = Vec::new();
         for line in instructions.into_iter() {
             let line = if line.address.value.properties.operand_imported {
-                let operand = line.operation.operand.value.try_into().unwrap();
-                let operation = if let Some(label) = import_map.remove(&operand) {
-                    let operand = Token::new(line.operation.operand.position, label.into());
-                    Operation {
-                        operand,
-                        ..line.operation
-                    }
-                } else {
-                    // TODO Add error treatment
-                    panic!("imported symbol not found")
+                let operand = match line.operation.operand.value {
+                    Operand::Numeric(immediate) => immediate,
+                    Operand::Symbolic(_) => panic!(),
                 };
+                let operand = if let Some(label) = import_map.get(&operand) {
+                    Token::new(line.operation.operand.position, label.clone().into())
+                } else {
+                    panic!("operand marked as imported but not imported");
+                };
+                let operation = Operation { operand, ..line.operation };
                 AddressedLine { operation, ..line }
             } else {
                 line
@@ -68,9 +68,11 @@ impl<'a> ProgramProcessor<'a> {
     }
 }
 
+#[derive(Debug)]
 pub struct ProgramsProcessor<'a> {
     pub linked_program: AddressedProgram<'a>,
     pub export_map: ExportMap<'a>,
+    pub inverted_import_map: ExportMap<'a>,
 }
 
 impl<'a> ProgramsProcessor<'a> {
@@ -82,15 +84,23 @@ impl<'a> ProgramsProcessor<'a> {
         let mut processed_programs: Vec<AddressedProgram> = Vec::new();
         let mut base: AddressPosition = 0;
         let mut export_map = ExportMap::new();
+        let mut imports = BTreeSet::<Label>::new();
         for program in programs {
             let processor = ProgramProcessor::process(base, program);
             base = processor.program.get_last_position() + 0x2;
             processed_programs.push(processor.program);
-            export_map.extend(processor.export_map)
+            Self::extend_export_unique(&mut export_map, processor.export_map);
+            imports.extend(processor.import_map.into_values());
         }
+        let inverted_import_map: BTreeMap<_, _> = imports
+            .into_iter()
+            .enumerate()
+            .filter(|(_, label)| !export_map.contains_key(label))
+            .map(|(i, label)| (label, u32::try_from(i).unwrap()))
+            .collect();
         let merged_program = Self::merge_programs(processed_programs);
-        let linked_program = Self::replace_imported_operands_with_positions(merged_program, &export_map);
-        ProgramsProcessor { linked_program, export_map }
+        let linked_program = Self::replace_imported_operands_with_positions(merged_program, &export_map, &inverted_import_map);
+        ProgramsProcessor { linked_program, export_map, inverted_import_map }
     }
 
     fn merge_programs(programs: Vec<AddressedProgram<'a>>) -> AddressedProgram<'a> {
@@ -101,21 +111,23 @@ impl<'a> ProgramsProcessor<'a> {
         AddressedProgram::new(lines)
     }
 
-    fn replace_imported_operands_with_positions(program: AddressedProgram<'a>, export_map: &ExportMap<'a>) -> AddressedProgram<'a> {
+    fn replace_imported_operands_with_positions(program: AddressedProgram<'a>, export_map: &ExportMap<'a>, inverted_import_map: &ExportMap<'a>) -> AddressedProgram<'a> {
         let mut lines: Vec<AddressedLine> = Vec::new();
         for line in program {
             let line = if line.address.value.properties.operand_imported {
-                let operand: Label = line.operation.operand.value.try_into().unwrap();
-                let operation = if let Some(position) = export_map.get(&operand) {
-                    let operand = Token::new(line.operation.operand.position, (*position).into());
-                    Operation {
-                        operand,
-                        ..line.operation
-                    }
-                } else {
-                    // TODO Add error treatment
-                    panic!("imported symbol not found")
+                let operand = match line.operation.operand.value {
+                    Operand::Numeric(_) => panic!(),
+                    Operand::Symbolic(label) => label,
                 };
+                let position = if let Some(position) = export_map.get(&operand) {
+                    position
+                } else if let Some(position) = inverted_import_map.get(&operand) {
+                    position
+                } else {
+                    panic!("operand marked as imported but not imported");
+                };
+                let operand = Token::new(line.operation.operand.position, (*position).into());
+                let operation = Operation { operand, ..line.operation };
                 AddressedLine { operation, ..line }
             } else {
                 line
@@ -123,5 +135,14 @@ impl<'a> ProgramsProcessor<'a> {
             lines.push(line);
         }
         AddressedProgram::new(lines)
+    }
+
+    fn extend_export_unique(original_map: &mut ExportMap<'a>, new_map: ExportMap<'a>) {
+        for (key, value) in new_map.into_iter() {
+            match original_map.insert(key, value) {
+                Some(_) => panic!("export map already contained label"),
+                None => continue,
+            }
+        }
     }
 }
