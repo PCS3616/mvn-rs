7b4240c4f87d8d8bae24895627f7897f3639302d
tomaz-suller <tomaz.suller@usp.br>
2023-02-26 10:15:25 -0300

    Integrate relocator into CLI

--- mvn-cli/src/main.rs
+++ mvn-cli/src/main.rs
@@ -25,7 +25,13 @@ enum Commands {
             action = clap::ArgAction::Append,
             value_parser = file_exists
         )]
-        inputs: Vec<PathBuf>
+        inputs: Vec<PathBuf>,
+    },
+    Relocate {
+        #[arg(short, long, value_parser = file_exists)]
+        input: PathBuf,
+        #[arg(short, long, value_parser = clap_num::maybe_hex::<u16>)]
+        base: u16,
     },
 }
 
@@ -33,19 +39,24 @@ fn main() {
     let cli = Cli::parse();
     match &cli.command {
         Commands::Assemble { input } => {
-            let program = fs::read_to_string(input).expect("failed to read file");
+            let program = read_to_string(input);
             let process_result = assembler::processor::process(&program);
             assembler::writer::print(&program, process_result);
         },
         Commands::Link { inputs } => {
             let programs: Vec<String> = inputs
                 .iter()
-                .map(|path| fs::read_to_string(path).expect("failed to read file"))
+                .map(|path| read_to_string(path))
                 .collect();
             let programs: Vec<&str> = programs.iter().map(String::as_str).collect();
             let process_result = linker::processor::process(programs);
             linker::writer::print(process_result);
         },
+        Commands::Relocate { input, base } => {
+            let program = read_to_string(input);
+            let process_result = relocator::processor::process(&program, *base);
+            relocator::writer::print(process_result);
+        }
     }
 }
 
@@ -57,4 +68,8 @@ fn file_exists(path: &str) -> Result<PathBuf, &'static str> {
         }
     }
     Err("input file does not exist")
+}
+
+fn read_to_string(path: &PathBuf) -> String {
+    fs::read_to_string(path).expect("failed to read file").to_uppercase()
 }
